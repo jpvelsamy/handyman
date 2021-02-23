dd8c9467379b5c89182d4cd12f0337c7b9110ea4
saucemaker <jana.poornavel@askjuno.com>
2021-02-23 11:22:52 +0530

    1. Added support for elastic search jdbc work 2. Fixed try catch throw problem in resource connection

--- handyman.parent/handyman/src/main/scala/in/handyman/util/ResourceAccess.scala
+++ handyman.parent/handyman/src/main/scala/in/handyman/util/ResourceAccess.scala
@@ -5,6 +5,13 @@ import in.handyman.config.ConfigurationService
 import in.handyman.config.Resource
 import java.sql.DriverManager
 
+/**
+ * 	Migration script added on Feb 19 2021 - 
+ * <ul>
+ * 	<li>alter table spw_resource_config add column driver_class varchar(100);</li>
+ *   <li>INSERT INTO spw_resource_config(config_type, config_name, auth_info, resource_url, active, created_date, user_name, password, driver_class)  values ('2','casey/schema_pim_kpi','','jdbc:sql4es://casey.store:9300/schema_pim_kpi?cluster.name=es-docker-cluster','Y',NOW(),'','','in.pazmysaz.essql.jdbc.ESDriver');</li>
+ *   </ul>
+ */
 
 object ResourceAccess {
   
@@ -12,10 +19,16 @@ object ResourceAccess {
   def rdbmsConn(name: String): Connection = 
   {
     val connResource: Resource = ConfigurationService.getResourceConfig(name)
+    if(connResource.driverClassName!=null)
+    {
+      Class.forName(connResource.driverClassName)
+    }
     val conn = DriverManager.
       getConnection(connResource.url,
         connResource.userName, connResource.password)
     conn.setAutoCommit(false)
     conn
   }
+  
+    
 }
