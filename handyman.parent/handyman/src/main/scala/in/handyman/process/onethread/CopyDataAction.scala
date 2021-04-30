03ff22567dc90f0dd656664f2615d2bf81a5584a
saucemaker <jana.poornavel@askjuno.com>
2021-04-30 20:58:18 +0530

    Dealt with type issues

--- handyman.parent/handyman/src/main/scala/in/handyman/process/onethread/CopyDataAction.scala
+++ handyman.parent/handyman/src/main/scala/in/handyman/process/onethread/CopyDataAction.scala
@@ -232,7 +232,7 @@ class CopyDataAction extends in.handyman.command.Action with LazyLogging {
       columnTypeName.toLowerCase match {
         case Constants.STRING_DATATYPE | "java.lang.string" => rs.getString(i)
         case "datetime" | "java.sql.date" => rs.getDate(i).toString()
-        case "timestamp" | "java.sql.timestamp" => rs.getTimestamp(i).toString()
+        case "timestamp" | "java.sql.timestamp" => rs.getLong(i).toString()
         case "double" | "java.lang.double" => rs.getDouble(i).toString()
         case "long" | "java.lang.long" => rs.getLong(i).toString()
         case "int" | "java.lang.integer" => rs.getInt(i).toString()
