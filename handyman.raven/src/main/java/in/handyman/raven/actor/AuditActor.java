5d144d5ca6b45fc255c3cf9bc7c8c4adbd64cc61
shankar prakash thirumaran <shankar.t@zucisystems.com>
2021-10-27 09:50:59 +0530

    redirection audit access

--- handyman.raven/src/main/java/in/handyman/raven/actor/AuditActor.java
+++ handyman.raven/src/main/java/in/handyman/raven/actor/AuditActor.java
@@ -1,44 +1,19 @@
 package in.handyman.raven.actor;
 
 import akka.actor.AbstractActor;
-import in.handyman.raven.audit.AuditPayload;
-import in.handyman.raven.audit.AuditService;
+import in.handyman.raven.access.AuditAccess;
+import in.handyman.raven.process.doa.ActionAudit;
+import in.handyman.raven.process.doa.PipelineAudit;
+import in.handyman.raven.process.doa.StatementAudit;
 
 public class AuditActor extends AbstractActor {
-
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match(AuditPayload.class, context -> {
-                    switch (context.getAuditType()) {
-                        case CREATE_INSTANCE_AUDIT:
-                            AuditService.insertInstanceAudit(context.getProcessId(),
-                                    context.getInstanceName(), context.getRunMode(),
-                                    context.getMachine(), context.getFile());
-                            break;
-                        case UPDATE_INSTANCE_AUDIT:
-                            AuditService.updateInstanceAudit(context.getProcessId(),
-                                    context.getStatus(), context.getContextLog(), context.getInstanceName());
-                            break;
-                        case CREATE_COMMAND_AUDIT:
-                            AuditService.insertCommandAudit(context.getActionId(),
-                                    context.getProcessId(), context.getActionName(),
-                                    context.getProcessName());
-                            break;
-                        case UPDATE_COMMAND_AUDIT:
-                            AuditService.updateCommandAudit(context.getActionId(), context.getStatus(), context.getCommandDetail());
-                            break;
-                        case CREATE_STATEMENT_AUDIT:
-                            AuditService.insertStatementAudit(context.getStatementId(), context.getActionId(), context.getActionName(), context.getProcessName());
-                            break;
-                        case UPDATE_STATEMENT_AUDIT:
-                            AuditService.updateStatementAudit(context.getStatementId(), context.getRowsW(), context.getRowsR(), context.getStatement(), context.getStatus());
-                            break;
-                        case CREATE_BATCH_AUDIT:
-                            //TODO
-                            break;
-                    }
-                }).build();
+                .match(PipelineAudit.class, AuditAccess::insertPipeline)
+                .match(ActionAudit.class, AuditAccess::insertAction)
+                .match(StatementAudit.class, AuditAccess::insertStatement)
+                .build();
     }
 }
