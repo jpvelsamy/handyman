a69915b648e82e80bd2b5963d04ba6ff01502bf0
Shankar Prakash T <shankarprakash32@gmail.com>
2021-12-21 21:26:32 +0530

    changes

--- handyman.raven/src/main/java/in/handyman/raven/lambda/access/repo/SpwProcessConfigRepo.java
+++ handyman.raven/src/main/java/in/handyman/raven/lambda/access/repo/SpwProcessConfigRepo.java
@@ -6,10 +6,14 @@ import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface SpwProcessConfigRepo {
 
-    @SqlUpdate("insert into " + SpwProcessConfig.SCHEMA_NAME + "." + SpwProcessConfig.TABLE_NAME + " (process, variable, value, active, created_by, created_date, last_modified_by, last_modified_date, version) VALUES ( :process, :variable, :value,:active,:createdBy, :createdDate, :lastModifiedBy, :lastModifiedDate, :version)")
+    @SqlUpdate("insert into " + SpwProcessConfig.SCHEMA_NAME + "." + SpwProcessConfig.TABLE_NAME + " (process, variable, value, active, created_by, created_date, last_modified_by, last_modified_date, version) " +
+            "VALUES ( :process, :variable, :value,:active,:createdBy, :createdDate, :lastModifiedBy, :lastModifiedDate, :version)")
     void insert(@BindBean final SpwProcessConfig spwProcessConfig);
-//
-//    @SqlUpdate("insert into " + SpwProcessConfig.SCHEMA_NAME + "." + SpwProcessConfig.TABLE_NAME + " (process, variable, value, active, created_by, created_date, last_modified_by, last_modified_date, version) VALUES ( :process, :variable, :value,:active,:createdBy, :createdDate, :lastModifiedBy, :lastModifiedDate, :version)")
-//    void insert(@BindBean final SpwProcessConfig spwProcessConfig);
+
+    @SqlUpdate("UPDATE " + SpwProcessConfig.SCHEMA_NAME + "." + SpwProcessConfig.TABLE_NAME + " SET active=:active, created_by= :createdBy , created_date= :createdDate," +
+            " last_modified_by= :lastModifiedBy, last_modified_date= :lastModifiedDate , version= :version WHERE process= :process AND variable= :variable AND value= :value ")
+    void update(@BindBean final SpwProcessConfig spwProcessConfig);
+
+
 
 }
