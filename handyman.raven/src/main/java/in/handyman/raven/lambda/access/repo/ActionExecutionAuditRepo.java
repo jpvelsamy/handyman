4e37c28c6f1fd32a1cd17d2ea288b474dd85c0cb
Praveen2918 <praveenkumar.m@zucisystems.com>
2022-01-13 22:29:58 +0530

    Fixed bean type binding issue in ActionExecutionAuditRepo PipelineExecutionAuditRepo

--- handyman.raven/src/main/java/in/handyman/raven/lambda/access/repo/ActionExecutionAuditRepo.java
+++ handyman.raven/src/main/java/in/handyman/raven/lambda/access/repo/ActionExecutionAuditRepo.java
@@ -4,6 +4,7 @@ package in.handyman.raven.lambda.access.repo;
 import in.handyman.raven.lambda.doa.audit.ActionExecutionAudit;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
@@ -14,14 +15,14 @@ public interface ActionExecutionAuditRepo {
     String COLUMNS = " action_id, created_by, created_date, last_modified_by, last_modified_date, context_node, execution_status_id, lambda_name, parent_action_id, parent_action_name, parent_pipeline_id, parent_pipeline_name, pipeline_name, action_name, execution_group_id, input_node, log, pipeline_id, root_pipeline_id ";
 
     @SqlUpdate("insert into " + ActionExecutionAudit.SCHEMA_NAME + "." + ActionExecutionAudit.TABLE_NAME + "  ( " + COLUMNS + " ) VALUES(:actionId, :createdBy, :createdDate, :lastModifiedBy, :lastModifiedDate, :contextNode, :executionStatusId, :lambdaName, :parentActionId, :parentActionName, :parentPipelineId, :parentPipelineName, :pipelineName, :actionName, :executionGroupId, :inputNode, :log, :pipelineId ,:rootPipelineId); ")
-    void insert(final ActionExecutionAudit actionExecutionAudit);
+    void insert(@BindBean final ActionExecutionAudit actionExecutionAudit);
 
     @SqlUpdate("update  " + ActionExecutionAudit.SCHEMA_NAME + "." + ActionExecutionAudit.TABLE_NAME + " SET created_by = :createdBy, created_date = :createdDate, last_modified_by = :lastModifiedBy," +
             " last_modified_date = :lastModifiedDate, context_node = :contextNode, execution_status_id = :executionStatusId, lambda_name = :lambdaName, " +
             "parent_action_id = :parentActionId, parent_action_name = :parentActionName, parent_pipeline_id = :parentPipelineId, " +
             "parent_pipeline_name = :parentPipelineName, pipeline_name = :pipelineName, action_name = :actionName, execution_group_id = :executionGroupId," +
             " input_node = :inputNode, log = :log, pipeline_id = :pipelineId,thread_name = :threadName, root_pipeline_id = :rootPipelineId  WHERE action_id = :actionId ;")
-    void update(final ActionExecutionAudit actionExecutionAudit);
+    void update(@BindBean final ActionExecutionAudit actionExecutionAudit);
 
 
     @SqlQuery("SELECT " + COLUMNS + " FROM  " + ActionExecutionAudit.SCHEMA_NAME + "." + ActionExecutionAudit.TABLE_NAME + " ; ")
--- handyman.raven/src/main/java/in/handyman/raven/lambda/access/repo/PipelineExecutionAuditRepo.java
+++ handyman.raven/src/main/java/in/handyman/raven/lambda/access/repo/PipelineExecutionAuditRepo.java
@@ -4,6 +4,7 @@ package in.handyman.raven.lambda.access.repo;
 import in.handyman.raven.lambda.doa.audit.PipelineExecutionAudit;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
@@ -15,10 +16,10 @@ public interface PipelineExecutionAuditRepo {
     String COLUMNS = " pipeline_id, created_by, created_date, last_modified_by, last_modified_date, context_node, execution_status_id, lambda_name, parent_action_id, parent_action_name, parent_pipeline_id, parent_pipeline_name, pipeline_name, file_content, host_name, mode_of_execution, pipeline_load_type, relative_path, request_body, thread_name, process_name, root_pipeline_id, process_id ";
 
     @SqlUpdate("insert into " + PipelineExecutionAudit.SCHEMA_NAME + "." + PipelineExecutionAudit.TABLE_NAME + "  ( " + COLUMNS + " ) VALUES( :pipelineId,:createdBy, :createdDate, :lastModifiedBy, :lastModifiedDate, :contextNode,:executionStatusId,:lambdaName,:parentActionId, :parentActionName, :parentPipelineId, :parentPipelineName, :pipelineName, :fileContent,:hostName,:modeOfExecution,:pipelineLoadType,:relativePath,:requestBody,:threadName,:processName,:rootPipelineId,:processId ); ")
-    void insert(final PipelineExecutionAudit actionExecutionAudit);
+    void insert(@BindBean final PipelineExecutionAudit actionExecutionAudit);
 
     @SqlUpdate("update  " + PipelineExecutionAudit.SCHEMA_NAME + "." + PipelineExecutionAudit.TABLE_NAME + " SET created_by = :createdBy, created_date = :createdDate, last_modified_by = :lastModifiedBy, last_modified_date = :lastModifiedDate, context_node = :contextNode, execution_status_id = :executionStatusId, lambda_name = :lambdaName, parent_action_id = :parentActionId, parent_action_name = :parentActionName, parent_pipeline_id = :parentPipelineId, parent_pipeline_name = :parentPipelineName, pipeline_name = :pipelineName, file_content = :fileContent, host_name = :hostName, mode_of_execution = :modeOfExecution, pipeline_load_type = :pipelineLoadType , relative_path = :relativePath, request_body = :requestBody,  process_name = :processName , root_pipeline_id = :rootPipelineId , process_id = :processId WHERE pipeline_id = :pipelineId ")
-    void update(final PipelineExecutionAudit actionExecutionAudit);
+    void update(@BindBean final PipelineExecutionAudit actionExecutionAudit);
 
 
     @SqlQuery("SELECT " + COLUMNS + " FROM  " + PipelineExecutionAudit.SCHEMA_NAME + "." + PipelineExecutionAudit.TABLE_NAME + " ; ")
