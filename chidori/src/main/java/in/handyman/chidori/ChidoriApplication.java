650a547ec67f1ca5ffae0c40013bd94be641c843
Mars <shankarprakash32@gmail.com>
2022-06-08 12:42:11 +0530

    rebased

---
+++ chidori/src/main/java/in/handyman/chidori/ChidoriApplication.java
@@ -0,0 +1,13 @@
+package in.handyman.chidori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChidoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChidoriApplication.class, args);
+	}
+
+}
