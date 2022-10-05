152158ac6a6e0026c2a3c6dc602ee095f71d3be3
DineshKrishnamoorthy1 <dinesh.krishna@zucisystems.com>
2022-10-05 12:26:31 +0530

    DocumentClassificationAction.java

--- raven/src/main/java/in/handyman/raven/lib/DocumentClassificationAction.java
+++ raven/src/main/java/in/handyman/raven/lib/DocumentClassificationAction.java
@@ -10,6 +10,7 @@ import in.handyman.raven.lib.model.DocumentClassification;
 import java.lang.Exception;
 import java.lang.Object;
 import java.lang.Override;
+import java.util.Collections;
 
 import okhttp3.*;
 import org.slf4j.Logger;
@@ -28,11 +29,13 @@ public class DocumentClassificationAction implements IActionExecution {
   private final Logger log;
 
   private final DocumentClassification documentClassification;
-  private final ObjectMapper mapper = new ObjectMapper();
-  private static final MediaType MediaTypeJSON = MediaType.parse("application/json; charset=utf-8");
-  private final String URI;
 
   private final Marker aMarker;
+
+  private final ObjectMapper mapper = new ObjectMapper();
+  private static final MediaType MediaTypeJSON = MediaType
+          .parse("application/json; charset=utf-8");
+  private final String URI  ;
 
   public DocumentClassificationAction(final ActionExecutionAudit action, final Logger log,
       final Object documentClassification) {
@@ -40,22 +43,32 @@ public class DocumentClassificationAction implements IActionExecution {
     this.action = action;
     this.log = log;
     this.aMarker = MarkerFactory.getMarker(" DocumentClassification:"+this.documentClassification.getName());
-    this.URI=action.getContext().get("copro.document-classification.url");
+    this.URI=action.getContext().get("copro.classification-document.url");
+
   }
 
   @Override
-  public void execute() throws Exception {{
+  public void execute() throws Exception {
+
     OkHttpClient httpclient = new OkHttpClient();
+
     final ObjectNode objectNode = mapper.createObjectNode();
+
     objectNode.put("inputFilePath",documentClassification.getFilePath());
     objectNode.put("outputDir",documentClassification.getOutputDir());
+    objectNode.put("modelFilePath",documentClassification.getModelFilePath());
+    objectNode.putPOJO("labels", Collections.singletonList(documentClassification.getLabels()));
+
+
+
     // build a request
     Request request = new Request.Builder().url(URI)
             .post(RequestBody.create( objectNode.toString(),MediaTypeJSON)).build();
-    log.info(aMarker, "The request got it successfully the File Path and outputDir {} {}",documentClassification.getFilePath()
-            ,documentClassification.getOutputDir());
+    log.info(aMarker, "The request got it successfully the File Path, outputDir, Model Path and Labels {} {} {} {}",documentClassification.getFilePath(),documentClassification.getOutputDir(),documentClassification.getModelFilePath(),documentClassification.getLabels());
+
     try (Response response = httpclient.newCall(request).execute()) {
       String responseBody = response.body().string();
+      System.out.println(responseBody);
       String name = documentClassification.getName() + "-document-classification-response";
       if (response.isSuccessful()) {
         action.getContext().put(name, responseBody);
@@ -67,7 +80,6 @@ public class DocumentClassificationAction implements IActionExecution {
       log.info(aMarker, "The Exception occurred ",e);
       throw new HandymanException("Failed to execute", e);
     }
-  }
   }
 
   @Override
