57992023c40146ee39fcefecb41709d53d3b4a36
SP-2827 <shankarprakash32@gmail.com>
2022-10-05 16:36:27 +0530

    add quartz.properties

--- raven/src/main/antlr/in.handyman.raven/compiler/RavenBaseListener.java
+++ raven/src/main/antlr/in.handyman.raven/compiler/RavenBaseListener.java
@@ -595,18 +595,6 @@ public class RavenBaseListener implements RavenListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterIncidentManagement(RavenParser.IncidentManagementContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitIncidentManagement(RavenParser.IncidentManagementContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
 	@Override public void enterResource(RavenParser.ResourceContext ctx) { }
 	/**
 	 * {@inheritDoc}
