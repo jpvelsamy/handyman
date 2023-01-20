60161d015f811ff03b3721a84dcd1cdbbf33b08c
Sanjeeya <sanjeeya.v@zucisystems.com>
2023-01-21 01:03:38 +0530

    Paper filter macros

--- src/main/java/in/handyman/raven/lib/ZeroShotClassifierPaperFilterAction.java
+++ src/main/java/in/handyman/raven/lib/ZeroShotClassifierPaperFilterAction.java
@@ -57,59 +57,55 @@ public class ZeroShotClassifierPaperFilterAction implements IActionExecution {
     @Override
     public void execute() throws Exception {
         final Jdbi jdbi = ResourceAccess.rdbmsJDBIConn(zeroShotClassifierPaperFilter.getResourceConn());
-
         log.info(aMarker, "<-------Phrase match paper filter Action for {} has been started------->", zeroShotClassifierPaperFilter.getName());
         final OkHttpClient httpclient = new OkHttpClient.Builder()
                 .connectTimeout(10, TimeUnit.MINUTES)
                 .writeTimeout(10, TimeUnit.MINUTES)
                 .readTimeout(10, TimeUnit.MINUTES)
                 .build();
-        JSONArray synonymContainerArray = new JSONArray(zeroShotClassifierPaperFilter.getKeysToFilter());
-        for (int i = 0; i < synonymContainerArray.length(); i++) {
-            final ObjectNode objectNode = mapper.createObjectNode();
-            objectNode.put("pageContent", zeroShotClassifierPaperFilter.getPageContent());
-            objectNode.set("keysToFilter", mapper.readTree(synonymContainerArray.get(i).toString()));
-            log.info(aMarker, " Input variables id : {}", action.getActionId());
-            Request request = new Request.Builder().url(URI)
-                    .post(RequestBody.create(objectNode.toString(), MediaTypeJSON)).build();
-            log.debug(aMarker, "Request has been build with the parameters \n URI : {} \n page content : {} \n key-filters : {} ", URI, zeroShotClassifierPaperFilter.getPageContent(), zeroShotClassifierPaperFilter.getKeysToFilter());
-            String name = zeroShotClassifierPaperFilter.getName() + "_response";
-            log.debug(aMarker, "The Request Details: {}", request);
-            try (Response response = httpclient.newCall(request).execute()) {
-                String responseBody = Objects.requireNonNull(response.body()).string();
-                if (response.isSuccessful()) {
-                    final String processId = Optional.ofNullable(zeroShotClassifierPaperFilter.getProcessID()).map(String::valueOf).orElse(null);
-                    JSONObject parentResponseObject = new JSONObject(responseBody);
-                    final Integer paperNo = Optional.ofNullable(zeroShotClassifierPaperFilter.getPaperNo()).map(String::valueOf).map(Integer::parseInt).orElse(null);
-                    JSONObject responseObject = new JSONObject(String.valueOf(parentResponseObject.get("entity_confidence_score")));
-                    responseObject.keys().forEachRemaining(key -> {
-                        ZeroShotClassifierPaperFilterAction.PaperFilteringZeroShotClassifierTable paperFilteringZeroShotClassifierTable = PaperFilteringZeroShotClassifierTable
-                                .builder()
-                                .originId(Optional.ofNullable(zeroShotClassifierPaperFilter.getOriginId()).map(String::valueOf).orElse(null))
-                                .groupId(Optional.ofNullable(zeroShotClassifierPaperFilter.getGroupId()).map(String::valueOf).orElse(null))
-                                .entity(Optional.ofNullable(key).map(String::valueOf).orElse(null))
-                                .confidenceScore(Optional.ofNullable(responseObject.get(key)).map(String::valueOf).orElse(null))
-                                .paperNo(paperNo)
-                                .build();
-                        jdbi.useTransaction(handle -> handle.createUpdate("INSERT INTO paper.zero_shot_classifier_filtering_result_" + processId + "(origin_id,group_id,paper_no,synonym,confidence_score, created_on)" +
-                                        " select :originId,:groupId,:paperNo,:entity,:confidenceScore,now();")
-                                .bindBean(paperFilteringZeroShotClassifierTable)
-                                .execute());
-                    });
-                    action.getContext().put(name.concat(".error"), "false");
-                    log.info(aMarker, "The Successful Response for {} --> {}", name, responseBody);
-                } else {
-                    action.getContext().put(name.concat(".error"), "true");
-                    action.getContext().put(name.concat(".errorMessage"), responseBody);
-                    log.info(aMarker, "The Failure Response {} --> {}", name, responseBody);
-                }
-                log.info(aMarker, "<-------Text Filtering Action for {} has been completed ------->", zeroShotClassifierPaperFilter.getName());
-            } catch (Exception e) {
+        final ObjectNode objectNode = mapper.createObjectNode();
+        objectNode.put("pageContent", zeroShotClassifierPaperFilter.getPageContent());
+        objectNode.set("keysToFilter", mapper.readTree(zeroShotClassifierPaperFilter.getKeysToFilter()));
+        log.info(aMarker, " Input variables id : {}", action.getActionId());
+        Request request = new Request.Builder().url(URI)
+                .post(RequestBody.create(objectNode.toString(), MediaTypeJSON)).build();
+        log.debug(aMarker, "Request has been build with the parameters \n URI : {} \n page content : {} \n key-filters : {} ", URI, zeroShotClassifierPaperFilter.getPageContent(), zeroShotClassifierPaperFilter.getKeysToFilter());
+        String name = zeroShotClassifierPaperFilter.getName() + "_response";
+        log.debug(aMarker, "The Request Details: {}", request);
+        try (Response response = httpclient.newCall(request).execute()) {
+            String responseBody = Objects.requireNonNull(response.body()).string();
+            if (response.isSuccessful()) {
+                final String processId = Optional.ofNullable(zeroShotClassifierPaperFilter.getProcessID()).map(String::valueOf).orElse(null);
+                JSONObject parentResponseObject = new JSONObject(responseBody);
+                final Integer paperNo = Optional.ofNullable(zeroShotClassifierPaperFilter.getPaperNo()).map(String::valueOf).map(Integer::parseInt).orElse(null);
+                JSONObject responseObject = new JSONObject(String.valueOf(parentResponseObject.get("entity_confidence_score")));
+                responseObject.keys().forEachRemaining(key -> {
+                    ZeroShotClassifierPaperFilterAction.PaperFilteringZeroShotClassifierTable paperFilteringZeroShotClassifierTable = PaperFilteringZeroShotClassifierTable
+                            .builder()
+                            .originId(Optional.ofNullable(zeroShotClassifierPaperFilter.getOriginId()).map(String::valueOf).orElse(null))
+                            .groupId(Optional.ofNullable(zeroShotClassifierPaperFilter.getGroupId()).map(String::valueOf).orElse(null))
+                            .entity(Optional.ofNullable(key).map(String::valueOf).orElse(null))
+                            .confidenceScore(Optional.ofNullable(responseObject.get(key)).map(String::valueOf).orElse(null))
+                            .paperNo(paperNo)
+                            .build();
+                    jdbi.useTransaction(handle -> handle.createUpdate("INSERT INTO paper.zero_shot_classifier_filtering_result_" + processId + "(origin_id,group_id,paper_no,synonym,confidence_score, created_on)" +
+                                    " select :originId,:groupId,:paperNo,:entity,:confidenceScore,now();")
+                            .bindBean(paperFilteringZeroShotClassifierTable)
+                            .execute());
+                });
+                action.getContext().put(name.concat(".error"), "false");
+                log.info(aMarker, "The Successful Response for {} --> {}", name, responseBody);
+            } else {
                 action.getContext().put(name.concat(".error"), "true");
-                action.getContext().put(name.concat(".errorMessage"), e.getMessage());
-                log.info(aMarker, "The Exception occurred ", e);
-                throw new HandymanException("Failed to execute", e);
+                action.getContext().put(name.concat(".errorMessage"), responseBody);
+                log.info(aMarker, "The Failure Response {} --> {}", name, responseBody);
             }
+            log.info(aMarker, "<-------Text Filtering Action for {} has been completed ------->", zeroShotClassifierPaperFilter.getName());
+        } catch (Exception e) {
+            action.getContext().put(name.concat(".error"), "true");
+            action.getContext().put(name.concat(".errorMessage"), e.getMessage());
+            log.info(aMarker, "The Exception occurred ", e);
+            throw new HandymanException("Failed to execute", e);
         }
     }
 
