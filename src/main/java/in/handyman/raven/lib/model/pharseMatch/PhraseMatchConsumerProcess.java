086c31d0baead14dc39c0dfcf017e8e898438a80
balasoundarya-thangapandi <balasoundarya.thanga@zucisystems.com>
2023-10-17 19:09:11 +0530

    file merger

--- src/main/java/in/handyman/raven/lib/model/pharseMatch/PhraseMatchConsumerProcess.java
+++ src/main/java/in/handyman/raven/lib/model/pharseMatch/PhraseMatchConsumerProcess.java
@@ -2,12 +2,16 @@ package in.handyman.raven.lib.model.pharseMatch;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import in.handyman.raven.exception.HandymanException;
 import in.handyman.raven.lambda.doa.audit.ActionExecutionAudit;
 import in.handyman.raven.lib.CoproProcessor;
+import in.handyman.raven.lib.model.fileMergerPdf.FileMergerpdfInputEntity;
+import in.handyman.raven.lib.model.fileMergerPdf.FileMergerpdfOutputEntity;
 import in.handyman.raven.lib.model.triton.TritonInputRequest;
 import in.handyman.raven.lib.model.triton.TritonRequest;
+import in.handyman.raven.lib.model.zeroShotClassifier.ZeroShotClassifierDataItem;
 import in.handyman.raven.util.ExceptionUtil;
 import okhttp3.*;
 import org.slf4j.Logger;
@@ -75,30 +79,69 @@ public class PhraseMatchConsumerProcess implements CoproProcessor.ConsumerProces
 
         String jsonRequest = objectMapper.writeValueAsString(requests);
 
-        try {
-            String truthPlaceholder = entity.getTruthPlaceholder();
+        String tritonRequestActivator = action.getContext().get("triton.request.activator");
 
-            log.info(aMarker, " Input variables id : {}", action.getActionId());
+
+        if (Objects.equals("true", tritonRequestActivator)) {
             Request request = new Request.Builder().url(endpoint)
-                    .post(RequestBody.create(jsonRequest.toString(), MediaTypeJSON)).build();
-
-            if (log.isInfoEnabled()) {
-                log.info(aMarker, "Input variables id : {}", actionId);
-                log.info(aMarker, "Request has been built with the parameters\nURI: {}, with truthPlaceHolder {}, originId {}, groupId {}, paperNo {}", endpoint, truthPlaceholder, originId, groupId, paperNo);
-            }
-            coproAPIProcessor(entity, parentObj, request);
-        } catch (Exception e) {
-            log.error("Error in the zero-shot classifier paper filter copro api call {}", e.toString());
-            HandymanException handymanException = new HandymanException(e);
-            HandymanException.insertException("Exception occurred in urgency triage model action for group id - " + groupId + " and originId - " + originId, handymanException, this.action);
+                    .post(RequestBody.create(data.toString(), MediaTypeJSON)).build();
+            coproRequestBuilder(entity,parentObj, request,objectMapper);
+        } else {
+            Request request = new Request.Builder().url(endpoint)
+                    .post(RequestBody.create(jsonRequest, MediaTypeJSON)).build();
+            tritonRequestBuilder(entity,parentObj,request);
         }
+
         return parentObj;
     }
 
-    private void coproAPIProcessor(PhraseMatchInputTable entity, List<PhraseMatchOutputTable> parentObj, Request request) throws JsonProcessingException {
-        String originId = entity.getOriginId();
-        String groupId = entity.getGroupId();
+    private void coproRequestBuilder(PhraseMatchInputTable entity, List<PhraseMatchOutputTable> parentObj, Request request, ObjectMapper objectMapper) {
+        final Integer paperNo = Optional.ofNullable(entity.getPaperNo()).map(String::valueOf).map(Integer::parseInt).orElse(null);
+        Long rootPipelineId = entity.getRootPipelineId();
+        try (Response response = httpclient.newCall(request).execute()) {
+            String responseBody = Objects.requireNonNull(response.body()).string();
+            if (response.isSuccessful()) {
 
+                            extractedOutputDataRequest(entity, parentObj, responseBody, objectMapper, "", "");
+
+                } else {
+                    parentObj.add(
+                            PhraseMatchOutputTable
+                                    .builder()
+                                    .originId(Optional.ofNullable(entity.getOriginId()).map(String::valueOf).orElse(null))
+                                    .groupId(Optional.ofNullable(entity.getGroupId()).map(String::valueOf).orElse(null))
+                                    .status("FAILED")
+                                    .paperNo(paperNo)
+                                    .stage(actionName)
+                                    .message(Optional.of(responseBody).map(String::valueOf).orElse(null))
+                                    .rootPipelineId(rootPipelineId)
+                                    .build());
+                    log.info(aMarker, "The Exception occurred in Phrase match API call");
+                }
+
+        } catch (Exception exception) {
+            parentObj.add(
+                    PhraseMatchOutputTable
+                            .builder()
+                            .originId(Optional.ofNullable(entity.getOriginId()).map(String::valueOf).orElse(null))
+                            .groupId(Optional.ofNullable(entity.getGroupId()).map(String::valueOf).orElse(null))
+                            .status("FAILED")
+                            .paperNo(paperNo)
+                            .stage(actionName)
+                            .message(exception.getMessage())
+                            .rootPipelineId(rootPipelineId)
+                            .build());
+            log.error(aMarker, "Exception occurred in the phrase match paper filter action {}", ExceptionUtil.toString(exception));
+            HandymanException handymanException = new HandymanException(exception);
+            HandymanException.insertException("Error in inserting Intellimatch result table", handymanException, this.action);
+
+        }
+    }
+
+
+
+
+    private void tritonRequestBuilder( PhraseMatchInputTable entity, List<PhraseMatchOutputTable> parentObj, Request request) {
 
         final Integer paperNo = Optional.ofNullable(entity.getPaperNo()).map(String::valueOf).map(Integer::parseInt).orElse(null);
         Long rootPipelineId = entity.getRootPipelineId();
@@ -110,26 +153,11 @@ public class PhraseMatchConsumerProcess implements CoproProcessor.ConsumerProces
                 PharseMatchResponse modelResponse = objectMapper.readValue(responseBody, PharseMatchResponse.class);
                 if (modelResponse.getOutputs() != null && !modelResponse.getOutputs().isEmpty()) {
                     modelResponse.getOutputs().forEach(o -> {
-                        o.getData().forEach(PharseMatchDataItem -> {
-                            parentObj.add(
-                                    PhraseMatchOutputTable
-                                            .builder()
-                                            .originId(Optional.ofNullable(originId).map(String::valueOf).orElse(null))
-                                            .groupId(Optional.ofNullable(groupId).map(String::valueOf).orElse(null))
-                                            .entity(Optional.ofNullable(entity).map(String::valueOf).orElse(null))
-                                            .paperNo(paperNo)
-                                            .status("COMPLETED")
-                                            .stage(actionName)
-                                            .message("Completed API call zero shot classifier")
-                                            .rootPipelineId(rootPipelineId)
-                                            .modelName(modelResponse.getModelName())
-                                            .modelVersion(modelResponse.getModelVersion())
-                                            .build());
-
+                        o.getData().forEach(pharseMatchDataItem -> {
+                            extractedOutputDataRequest(entity, parentObj, pharseMatchDataItem, objectMapper, "", "");
                         });
                     });
-                }
-            } else {
+                } else {
                 parentObj.add(
                         PhraseMatchOutputTable
                                 .builder()
@@ -142,6 +170,7 @@ public class PhraseMatchConsumerProcess implements CoproProcessor.ConsumerProces
                                 .rootPipelineId(rootPipelineId)
                                 .build());
                 log.info(aMarker, "The Exception occurred in Phrase match API call");
+            }
             }
         } catch (Exception exception) {
             parentObj.add(
@@ -161,6 +190,37 @@ public class PhraseMatchConsumerProcess implements CoproProcessor.ConsumerProces
 
         }
     }
-}
 
+    private static void extractedOutputDataRequest(PhraseMatchInputTable entity, List<PhraseMatchOutputTable> parentObj, String pharseMatchDataItem, ObjectMapper objectMapper, String modelName, String modelVersion) {
+        String originId = entity.getOriginId();
+        String groupId = entity.getGroupId();
+
+
+        final Integer paperNo = Optional.ofNullable(entity.getPaperNo()).map(String::valueOf).map(Integer::parseInt).orElse(null);
+        Long rootPipelineId = entity.getRootPipelineId();
+        try {
+            PharseMatchDataItem pharseMatchOutputData = objectMapper.readValue(pharseMatchDataItem, PharseMatchDataItem.class);
+
+            parentObj.add(
+                    PhraseMatchOutputTable
+                            .builder()
+                            .originId(Optional.ofNullable(originId).map(String::valueOf).orElse(null))
+                            .groupId(Optional.ofNullable(groupId).map(String::valueOf).orElse(null))
+                            .paperNo(paperNo)
+                            .status("COMPLETED")
+                            .stage(actionName)
+                            .message("Completed API call zero shot classifier")
+                            .rootPipelineId(rootPipelineId)
+                            .modelName(modelName)
+                            .truthEntity(pharseMatchOutputData.getTruthEntity())
+                            .isKeyPresent(pharseMatchOutputData.getIsKeyPresent())
+                            .entity(pharseMatchOutputData.getEntity())
+                            .modelVersion(modelVersion)
+                            .build());
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
 
