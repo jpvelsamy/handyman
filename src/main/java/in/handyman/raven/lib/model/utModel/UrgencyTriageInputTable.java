cce687911a09b4492f8d536ff77e0e0ffb3bc6ef
anandh2001 <anandh.andrews@zucisystems.com>
2023-10-27 15:13:50 +0530

    urgency triage cleanup for tenant_id and url addition in adapters

--- src/main/java/in/handyman/raven/lib/model/utModel/UrgencyTriageInputTable.java
+++ src/main/java/in/handyman/raven/lib/model/utModel/UrgencyTriageInputTable.java
@@ -8,26 +8,26 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Data
-    @Builder
-    public class UrgencyTriageInputTable implements CoproProcessor.Entity {
-        private String createdUserId;
-        private String lastUpdatedUserId;
-        private Long tenantId;
-        private Long processId;
-        private Integer groupId;
-        private String originId;
-        private Integer paperNo;
-        private String templateId;
-        private Long modelId;
-        private String inputFilePath;
-        private Long rootPipelineId;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class UrgencyTriageInputTable implements CoproProcessor.Entity {
+    private String createdUserId;
+    private String lastUpdatedUserId;
+    private Long tenantId;
+    private Long processId;
+    private Integer groupId;
+    private String originId;
+    private Integer paperNo;
+    private String templateId;
+    private Long modelId;
+    private String inputFilePath;
+    private Long rootPipelineId;
 
-        @Override
-        public List<Object> getRowData() {
-            return null;
-        }
+    @Override
+    public List<Object> getRowData() {
+        return null;
     }
+}
 
