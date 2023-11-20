6dce858d69dad8c3fb9044b109b5a6ae7baf5f7c
christo0306 <christopher.paulraj@zucisystems.com>
2023-11-21 01:56:38 +0530

    noise detection model macro
    Outbound release

---
+++ src/main/java/in/handyman/raven/lib/model/integratedNoiseModel/NoiseModelData.java
@@ -0,0 +1,25 @@
+package in.handyman.raven.lib.model.integratedNoiseModel;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NoiseModelData {
+
+        private Long rootPipelineId;
+        private Long actionId;
+        private String process;
+        private String inputFilePath;
+        private String outputDir;
+        private String originId;
+        private Integer paperNo;
+        private Long processId;
+        private Integer groupId;
+        private Long tenantId;
+}
